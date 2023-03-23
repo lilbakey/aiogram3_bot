3dc8862eccea660830f67b291aecbb0ce9122c61
Marshall <alexbakeev07@gmail.com>
2023-03-23 17:36:43 +0300

    refactoring

--- filters/is_faq_content.py
+++ filters/is_faq_content.py
@@ -6,5 +6,5 @@ from aiogram.filters import BaseFilter
 
 class IsFAQContent(BaseFilter):
     async def __call__(self, message: Message) -> bool:
-        help_content: str = await FAQ.get_help_content()
+        help_content: str = await FAQ.get_faq_content()
         return True if help_content else False
